3d446726ae63f731baf219ff7ab71b7681f0f7d3
rahulreddy811 <srahulreddy424@gmail.com>
2026-03-23 10:06:26 +0530

    mapping added

--- src/main/java/com/example/demo/Controller/TorFController.java
+++ src/main/java/com/example/demo/Controller/TorFController.java
@@ -5,6 +5,7 @@ import com.example.demo.model.TorFModel;
 import com.example.demo.service.TorFService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,7 @@ public class TorFController {
     @Autowired
     private TorFService service;
 
+    @GetMapping
     public List<TorFModel> getAll(){
         return service.getAll();
     }
